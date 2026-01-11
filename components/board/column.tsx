c8b7416195d8a0c662e41a827b656279abf4fa89
Spinu Alex <spinualexandru@outlook.com>
2026-01-11 23:04:25 +0200

    feat: default sort

--- components/board/column.tsx
+++ components/board/column.tsx
@@ -64,7 +64,7 @@ export function Column({
 			</Box>
 			<Box flexGrow={1} paddingX={1} flexDirection="column">
 				<CardList
-					tasks={tasks}
+					tasks={tasks.sort((a, b) => new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime())}
 					selectedIndex={selectedIndex}
 					isFocused={isFocused}
 					useNerdfonts={useNerdfonts}
